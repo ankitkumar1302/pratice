f79160774fcd3236b65c539f4dc95bc95da9380d
ankit <you@example.com>
2023-02-12 22:29:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pratice/KotlinBasics.kt
@@ -0,0 +1,70 @@
+package com.example.pratice
+
+fun main() {
+
+//    val num = arrayOf(1, 2, 3, 4, 5, 6, 7, 8)
+//    println(num.contentToString())
+//    print(num[0])
+//    val numD = arrayOf(1.0, 1.2, 1.3)
+//    val charD = arrayOf("asdfa", "sadfsdaf", "asdfsdafa")
+//
+////        print(charD.contentToString())
+//
+//    val fruits = arrayOf(Fruits("Apple",20),Fruits("grapes",10))
+//    print(fruits.contentToString())
+//    for (index in fruits.indices){
+//        print("\n${fruits[index].Name} is in Index $index")
+//    }
+//    for (fruits in fruits){
+//        print("\n"+fruits.Name)
+//    }
+
+    // ListOff
+
+//    val months = listOf("jan","feb","March")
+//    println(months.size)
+//    print(months[0])
+//    for (months in months){
+//        println(months)
+//    }
+
+//    val additionalMonths = months.toMutableList()
+//    val newMonths = arrayOf("April","May","June")
+//    additionalMonths.addAll(newMonths)
+//    additionalMonths.add("July")
+//    print(additionalMonths)
+
+    // Mutable List off
+//    val days = mutableListOf<String>("Mon","Tue","Wed")
+//    days.add("Thu")
+//    println(days)
+
+    //Sets
+
+//    val fruits = setOf<String>("Orange","Apples","Grapes","Apples")
+//    println(fruits.toSortedSet())
+//
+//    val newFruits = fruits.toMutableList()
+//    newFruits.add("Water melon")
+//    newFruits.add("peach")
+//    println(newFruits.elementAt(0))
+//
+//    //Maps
+//
+//    val dayOfWeek = mapOf(1 to "Monday",2 to "Tuesday",3 to "Wednesday")
+//    for (key in dayOfWeek){
+//        println(key)
+//    }
+
+    // ArrayList
+//
+//    val arrayList = arrayListOf<Any>()
+//    arrayList.add("One")
+//    arrayList.add("Two")
+//    println("....Print ArrayList.....")
+//    for (i in arrayList){
+//        println(i)
+//    }
+
+}
+//data class Fruits(val Name:String,val Price:Int)
